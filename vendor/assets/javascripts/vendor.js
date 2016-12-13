1ccd3cdd00026cef6cc21332391f24f022b0965e
Thomas Rogers <thomasrogers03@gmail.com>
2016-12-13 18:41:44 -0500

    Add jointjs to the project

--- vendor/assets/javascripts/vendor.js
+++ vendor/assets/javascripts/vendor.js
@@ -1,5 +1,8 @@
+//= require bootstrap
+//= require lodash
+//= require backbone
+//= require joint
 //= require codemirror
 //= require codemirror-ruby
 //= require codemirror-javascript
 //= require codemirror-yaml
-//= require bootstrap
