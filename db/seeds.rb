bb7e0fc370c425f4e809f1f80717b1087f488c98
Thomas Rogers <thomasrogers03@gmail.com>
2016-12-12 20:18:24 -0500

    More seed jobs/nodes

--- db/seeds.rb
+++ db/seeds.rb
@@ -43,6 +43,28 @@ Job.transaction do
 }
   Job.create!(job_name: 'Test Job 2', job_script_attributes: {script: job_two_script})
 
+  job_three_script = %q{---
+:root: run_this
+:nodes:
+  run_this:
+    :type: SetConstant
+    :outputs:
+      :target: :url
+    :properties:
+      :value: http://www.example.com
+    :next_nodes:
+      - run_this_next
+  run_this_next:
+    :type: PretendVisitPage
+    :inputs:
+      :url: :url
+    :outputs:
+      :page_content: :page_content
+}
+  Job.create!(job_name: 'Visit Example Website', job_script_attributes: {script: job_three_script})
+
+
+
   NodeDefinition.create!(name: 'DebugNode', script: %q{puts "=> DEBUG: #{context.values.inspect}"}, meta_data: "---\n")
 
   dummy_meta_data = %q{---
@@ -59,5 +81,21 @@ Job.transaction do
 :properties:
   :job: :string
 }
-  NodeDefinition.create!(name: 'RunJob', script: 'Job.find_by!(job_name: job).script.run(context)', meta_data: run_job_meta_data)
+  NodeDefinition.create!(name: 'RunJob', script: 'Job.find_by!(job_name: job).job_script.run(context)', meta_data: run_job_meta_data)
+
+  visit_page_meta_data = %q{---
+:outputs:
+  - :page_content
+:inputs:
+  - :url
+}
+  NodeDefinition.create!(name: 'VisitPage', script: 'context.values[page_content] = URI.parse(context.values[url]).open.read', meta_data: visit_page_meta_data)
+
+  fake_visit_page_meta_data = %q{---
+:outputs:
+  - :page_content
+:inputs:
+  - :url
+}
+  NodeDefinition.create!(name: 'PretendVisitPage', script: 'context.values[page_content] = "Content from #{context.values[url]}"', meta_data: fake_visit_page_meta_data)
 end
